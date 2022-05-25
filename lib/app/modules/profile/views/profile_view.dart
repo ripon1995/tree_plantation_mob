e73443fe7d9436e37a7ad3a6b41eccea8b9294b5
Ripon <alamin.ripon@brainstation-23.com>
2022-05-26 03:41:49 +0600

    why : login page issue fixed
    how : get_cli used to create page

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -9,41 +9,43 @@ class ProfileView extends GetView<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.only(top: 100),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 100,
-                  width: 400,
-                  child: Align(
-                      alignment: Alignment.topRight,
-                      child: SvgPicture.asset("assets/images/app_logo.svg")),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Welcome Back!",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Container(
+              margin: const EdgeInsets.only(top: 100),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 100,
+                    width: 400,
+                    child: Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset("assets/images/app_logo.svg")),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                customTextField("username", Icons.person_outlined, 20),
-                customTextField("mobile", Icons.phone_android_outlined, 11),
-                customTextField("email", Icons.email_outlined, 20),
-                customTextField("password", Icons.password_outlined, 20),
-                customElevatedButton("Next"),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Welcome Back!",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  customTextField("username", Icons.person_outlined, 20),
+                  customTextField("mobile", Icons.phone_android_outlined, 11),
+                  customTextField("email", Icons.email_outlined, 20),
+                  customTextField("password", Icons.password_outlined, 20),
+                  customElevatedButton("Next"),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
             ),
           ),
         ));
