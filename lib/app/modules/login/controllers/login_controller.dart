4e069a16a8f546196ac11558dd3baed8599cedcd
rootless <alamin.ripon@brainstation-23.com>
2022-07-01 20:02:53 +0600

    why ? 1. Login process implementation
    how ?
    1. controller implementation done.

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tree_plantation_mobile/app/data/model/request/login_request.dart';
+import 'package:tree_plantation_mobile/app/data/repository/auth_repository.dart';
+import 'package:tree_plantation_mobile/app/data/repository/auth_repository_impl.dart';
+import 'package:tree_plantation_mobile/app/log.dart';
 
 class LoginController extends GetxController {
+
+  AuthRepository _authRepository = AuthRepositoryImpl();
+
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -21,7 +28,14 @@ class LoginController extends GetxController {
   void onClose() {}
 
   void login() {
-    String username = emailController.text.toString();
+    String email = emailController.text.toString();
     String password = passwordController.text.toString();
+
+    LoginRequest loginRequest = LoginRequest(email, password);
+    var loginResponse = _authRepository.login(loginRequest);
+    loginResponse.then((value) {
+      Log.debug("Access token : ${value.accessToken}");
+      Log.debug("Refresh token : ${value.refreshToken}");
+    });
   }
 }
