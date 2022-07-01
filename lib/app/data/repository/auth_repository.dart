d877821418c7a0c4e4f2d1ebc72cd46c7c0f910d
rootless <alamin.ripon@brainstation-23.com>
2022-07-01 19:57:53 +0600

    why ? 1. Login process implementation
    how ?
    1. Repository implementation done.

--- lib/app/data/repository/auth_repository.dart
+++ lib/app/data/repository/auth_repository.dart
@@ -1,6 +1,10 @@
+import 'package:tree_plantation_mobile/app/data/model/request/login_request.dart';
 import 'package:tree_plantation_mobile/app/data/model/request/sign_up_request.dart';
+import 'package:tree_plantation_mobile/app/data/model/response/login_response.dart';
 import 'package:tree_plantation_mobile/app/data/model/response/sign_up_response.dart';
 
 abstract class AuthRepository {
   Future<SignUpResponse> signUp(SignUpRequest signUpRequest);
+
+  Future<LoginResponse> login(LoginRequest loginRequest);
 }
--- lib/app/data/repository/auth_repository_impl.dart
+++ lib/app/data/repository/auth_repository_impl.dart
@@ -1,4 +1,6 @@
+import 'package:tree_plantation_mobile/app/data/model/request/login_request.dart';
 import 'package:tree_plantation_mobile/app/data/model/request/sign_up_request.dart';
+import 'package:tree_plantation_mobile/app/data/model/response/login_response.dart';
 import 'package:tree_plantation_mobile/app/data/model/response/sign_up_response.dart';
 import 'package:tree_plantation_mobile/app/data/remote/auth_remote_source.dart';
 import 'package:tree_plantation_mobile/app/data/remote/auth_remote_source_impl.dart';
@@ -10,5 +12,10 @@ class AuthRepositoryImpl extends AuthRepository {
   @override
   Future<SignUpResponse> signUp(SignUpRequest signUpRequest) {
     return _authRemoteSource.signUp(signUpRequest);
+  }
+
+  @override
+  Future<LoginResponse> login(LoginRequest loginRequest) {
+    return _authRemoteSource.login(loginRequest);
   }
 }
