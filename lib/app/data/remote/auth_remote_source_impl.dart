652f1b8042f2cd2c3c89061e428c1ff404d33141
rootless <alamin.ripon@brainstation-23.com>
2022-07-01 19:56:13 +0600

    why ? 1. Login process implementation
    how ?
    1. Remote source implementation done.

--- lib/app/data/remote/auth_remote_source_impl.dart
+++ lib/app/data/remote/auth_remote_source_impl.dart
@@ -2,18 +2,22 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:tree_plantation_mobile/app/core/base/base_remote_source.dart';
+import 'package:tree_plantation_mobile/app/data/model/request/login_request.dart';
 import 'package:tree_plantation_mobile/app/data/model/request/sign_up_request.dart';
+import 'package:tree_plantation_mobile/app/data/model/response/login_response.dart';
 import 'package:tree_plantation_mobile/app/data/model/response/sign_up_response.dart';
 import 'package:tree_plantation_mobile/app/data/remote/auth_remote_source.dart';
 import 'package:tree_plantation_mobile/app/network/dio_provider.dart';
 
 class AuthRemoteSourceImpl extends BaseRemoteSource
     implements AuthRemoteSource {
+  // Sign up start
+
   @override
   Future<SignUpResponse> signUp(SignUpRequest signUpRequest) {
     var endpoint = "${DioProvider.baseUrl}/customers/signup/";
     String json = jsonEncode(signUpRequest);
-    var dioCall = dioClient.post(endpoint,data: json);
+    var dioCall = dioClient.post(endpoint, data: json);
 
     try {
       return callApiWithErrorParser(dioCall)
@@ -26,4 +30,27 @@ class AuthRemoteSourceImpl extends BaseRemoteSource
   SignUpResponse _parseSignUpResponse(Response<dynamic> response) {
     return SignUpResponse.fromJson(response.data);
   }
+
+  @override
+  Future<LoginResponse> login(LoginRequest loginRequest) {
+    var endpoint = "${DioProvider.baseUrl}/auth/jwt/create/";
+    String json = jsonEncode(loginRequest);
+
+    var dioCall = dioClient.post(endpoint, data: json);
+    try {
+      return callApiWithErrorParser(dioCall)
+          .then((response) => _parseLoginResponse(response));
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  LoginResponse _parseLoginResponse(Response<dynamic> response) {
+    return LoginResponse.fromJson(response.data);
+  }
+
+// sign up end
+
+// Login start
+
 }
