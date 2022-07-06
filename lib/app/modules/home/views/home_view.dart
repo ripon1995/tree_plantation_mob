0a0b640d998c48f36b6eeba1a79f10d4b321a905
rootless <alamin.ripon@brainstation-23.com>
2022-07-07 03:42:40 +0600

    task : 2-home
    what ?
    home refactored started
    
    how ?
    1. Ui changed

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -13,7 +13,7 @@ class HomeView extends GetView<HomeController> {
       ),
       body: Center(
         child: Text(
-          'HomeView is working',
+          'Welcome Home',
           style: TextStyle(fontSize: 20),
         ),
       ),
