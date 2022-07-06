53c684517f68f40100264327fc79e309f1242d97
rootless <alamin.ripon@brainstation-23.com>
2022-07-07 02:42:36 +0600

    why ? 1. Login process implementation
    how ?
    1. after successful login accesstoken and refreshtoken are saved in the preference

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tree_plantation_mobile/app/data/local/preference/preference_manager.dart';
 import 'package:tree_plantation_mobile/app/data/model/request/login_request.dart';
 import 'package:tree_plantation_mobile/app/data/repository/auth_repository.dart';
 import 'package:tree_plantation_mobile/app/data/repository/auth_repository_impl.dart';
 import 'package:tree_plantation_mobile/app/log.dart';
 
 class LoginController extends GetxController {
-
   AuthRepository _authRepository = AuthRepositoryImpl();
+  PreferenceManager _preferenceManager = Get.find();
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -34,8 +35,11 @@ class LoginController extends GetxController {
     LoginRequest loginRequest = LoginRequest(email, password);
     var loginResponse = _authRepository.login(loginRequest);
     loginResponse.then((value) {
-      Log.debug("Access token : ${value.accessToken}");
-      Log.debug("Refresh token : ${value.refreshToken}");
+      _preferenceManager.setString("accessToken", value.accessToken!);
+      _preferenceManager.setString("refreshToken", value.refreshToken!);
+      Log.debug("Token saved in preference manager");
+      Log.debug("Access token : ${_preferenceManager.getString("accessToken")}");
+      Log.debug("Refresh token : ${_preferenceManager.getString("refreshToken")}");
     });
   }
 }
