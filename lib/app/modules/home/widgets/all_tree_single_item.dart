20e75ecdbcd0406a1595a94cf223151f63f7396f
rootless <alamin.ripon@brainstation-23.com>
2022-07-08 12:55:06 +0600

    task : 2-home
    what ?
    ui implementation
    
    how ?
    1. all-trees single item card color changed

--- lib/app/modules/home/widgets/all_tree_single_item.dart
+++ lib/app/modules/home/widgets/all_tree_single_item.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 Widget allTreeSingleItem(String imageLink) {
+  const color = const Color(0xfffaf9ff);
   return Card(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(30),
     ),
-    color: Colors.white70,
+    color: color,
     child: Image.asset(imageLink),
   );
 }
