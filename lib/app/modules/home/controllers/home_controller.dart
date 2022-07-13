915083b1cbcdf6fad22cbd89e8bb795e46d2508b
rootless <alamin.ripon@brainstation-23.com>
2022-07-14 04:19:31 +0600

    task : 2-home
    what ?
    profile data fetch
    
    how ?
    1. implemented controller for fetching profile data and store in shared preference

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
+import 'package:tree_plantation_mobile/app/data/local/preference/preference_manager.dart';
+import 'package:tree_plantation_mobile/app/data/model/response/user_profile.dart';
+import 'package:tree_plantation_mobile/app/data/repository/auth_repository.dart';
+import 'package:tree_plantation_mobile/app/log.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final AuthRepository _authRepository =
+      Get.find(tag: (AuthRepository).toString());
+  final PreferenceManager _preferenceManager =
+      Get.find(tag: (PreferenceManager).toString());
 
   final count = 0.obs;
+
   @override
   void onInit() {
     super.onInit();
@@ -14,7 +22,27 @@ class HomeController extends GetxController {
     super.onReady();
   }
 
+  void getProfile() async {
+    UserProfile? profile = await _authRepository.userProfile();
+    if (profile.detail?.id != null) {
+      await _preferenceManager.setString(
+          PreferenceManager.email, profile.detail!.email.toString());
+      await _preferenceManager.setString(
+          PreferenceManager.name, profile.detail!.name.toString());
+    }
+    printData();
+  }
+
+  void printData() async {
+    String name = await _preferenceManager.getString(PreferenceManager.name);
+    String email = await _preferenceManager.getString(PreferenceManager.email);
+    Log.debug("Fetching user profile from preference");
+    Log.debug("name : $name");
+    Log.debug("email : $email");
+  }
+
   @override
   void onClose() {}
+
   void increment() => count.value++;
 }
