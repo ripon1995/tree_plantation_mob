22bca4fabc83f621d2402ed735c3509b7096c141
rootless <alamin.ripon@brainstation-23.com>
2022-07-13 20:10:33 +0600

    task : 2-home
    what ?
    profile data fetch
    
    how ?
    1. auth remote source implemented

--- lib/app/data/remote/auth_remote_source.dart
+++ lib/app/data/remote/auth_remote_source.dart
@@ -2,9 +2,12 @@ import 'package:tree_plantation_mobile/app/data/model/request/login_request.dart
 import 'package:tree_plantation_mobile/app/data/model/request/sign_up_request.dart';
 import 'package:tree_plantation_mobile/app/data/model/response/login_response.dart';
 import 'package:tree_plantation_mobile/app/data/model/response/sign_up_response.dart';
+import 'package:tree_plantation_mobile/app/data/model/response/user_profile.dart';
 
 abstract class AuthRemoteSource {
   Future<SignUpResponse> signUp(SignUpRequest signUpRequest);
 
   Future<LoginResponse> login(LoginRequest loginRequest);
+
+  Future<UserProfile> userProfile();
 }
